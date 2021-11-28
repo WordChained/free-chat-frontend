b816f6488cec4ded1a2dbf54155b993b4c50a7ad
Tal Ben David <talbd51@gmail.com>
2021-11-28 11:59:16 +0200

    adding 'api' in the base url

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -2,7 +2,7 @@ import Axios from 'axios'
 // import { router } from '@/router'
 
 const BASE_URL = process.env.NODE_ENV === 'production'
-    ? '//free-chat-1.herokuapp.com/'
+    ? '//free-chat-1.herokuapp.com/api/'
     : '//localhost:3030/api/'
 
 
