d2b726751fdee6f4711e2bedf266d18eff9fe987
Tal Ben David <talbd51@gmail.com>
2021-11-28 14:27:04 +0200

    printing the port

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,6 +1,7 @@
 import Axios from 'axios'
 // import { router } from '@/router'
 console.log('process.env.NODE_ENV:', process.env.NODE_ENV)
+console.log('process.env.PORT:', process.env.PORT)
 const BASE_URL = process.env.NODE_ENV === 'production'
     ? '/'
     : '//localhost:3030/api/'
