19a7312e012fc0ff6f8168680285e56a1b03fa7d
Tal Ben David <talbd51@gmail.com>
2022-04-05 19:38:14 +0300

    some css adjustments

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,28 +9,30 @@ import nodetIcon from '../assets/technoligies/node.png';
 import sasstIcon from '../assets/technoligies/sass.png';
 export const About = () => {
   return (
-    <div className="about-page">
-      <h2>About Free-Chat</h2>
-      <p>Hello and welcome to my Free-Chat app!</p>
-      <p>
-        Remember to be respectful to the other users and keep everything as
-        civil as possible. This app was created to showcase a few of my
-        abilities.
-      </p>
-      <p>
-        You can either chat with random people by topics (like Omegle), go
-        topic-less or go to the chat rooms. if you have an idea for a room, feel
-        free to add one!
-      </p>
-      <p>These are some of the technologies i used:</p>
-      <div className="icons">
-        <img src={reactIcon} alt="react" />
-        <img src={reduxIcon} alt="redux" />
-        <img src={html5Icon} alt="html5" />
-        <img src={css3Icon} alt="css3" />
-        <img src={mongoIcon} alt="mongoDB" />
-        <img src={nodetIcon} alt="node.js" />
-        <img src={sasstIcon} alt="sass" />
+    <div className='about-page'>
+      <div className='about-text'>
+        <h2>About Free-Chat</h2>
+        <p>Hello and welcome to my Free-Chat app!</p>
+        <p>
+          Remember to be respectful to the other users and keep everything as
+          civil as possible. This app was created to showcase a few of my
+          abilities.
+        </p>
+        <p>
+          You can either chat with random people by topics (like Omegle), go
+          topic-less or go to the chat rooms. if you have an idea for a room,
+          feel free to add one!
+        </p>
+        <p>These are some of the technologies i used:</p>
+      </div>
+      <div className='icons'>
+        <img src={reactIcon} alt='react' />
+        <img src={reduxIcon} alt='redux' />
+        <img src={html5Icon} alt='html5' />
+        <img src={css3Icon} alt='css3' />
+        <img src={mongoIcon} alt='mongoDB' />
+        <img src={nodetIcon} alt='node.js' />
+        <img src={sasstIcon} alt='sass' />
       </div>
     </div>
   );
